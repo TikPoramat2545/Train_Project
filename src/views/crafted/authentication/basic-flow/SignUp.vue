f3109a737bdbf58c8ae13709f63090a8fa8d52a6
Poramat <porramut255780@gmail.com>
2023-05-29 13:12:30 +0700

    latest

--- src/views/crafted/authentication/basic-flow/SignUp.vue
+++ src/views/crafted/authentication/basic-flow/SignUp.vue
@@ -47,42 +47,20 @@
       <!--end::Separator-->
 
       <!--begin::Input group-->
-      <div class="row fv-row mb-7">
-        <!--begin::Col-->
-        <div class="col-xl-6">
-          <label class="form-label fw-bold text-dark fs-6">First Name</label>
-          <Field
-            class="form-control form-control-lg form-control-solid"
-            type="text"
-            placeholder=""
-            name="first_name"
-            autocomplete="off"
-          />
-          <div class="fv-plugins-message-container">
-            <div class="fv-help-block">
-              <ErrorMessage name="first_name" />
-            </div>
+      <div class="fv-row mb-7">
+        <label class="form-label fw-bold text-dark fs-6">Username</label>
+        <Field
+          class="form-control form-control-lg form-control-solid"
+          type="username"
+          placeholder=""
+          name="username"
+          autocomplete="off" v-model="username"
+        />
+        <div class="fv-plugins-message-container">
+          <div class="fv-help-block">
+            <ErrorMessage name="username" />
           </div>
         </div>
-        <!--end::Col-->
-
-        <!--begin::Col-->
-        <div class="col-xl-6">
-          <label class="form-label fw-bold text-dark fs-6">Last Name</label>
-          <Field
-            class="form-control form-control-lg form-control-solid"
-            type="text"
-            placeholder=""
-            name="last_name"
-            autocomplete="off"
-          />
-          <div class="fv-plugins-message-container">
-            <div class="fv-help-block">
-              <ErrorMessage name="last_name" />
-            </div>
-          </div>
-        </div>
-        <!--end::Col-->
       </div>
       <!--end::Input group-->
 
@@ -94,7 +72,8 @@
           type="email"
           placeholder=""
           name="email"
-          autocomplete="off"
+          autocomplete="off" 
+          v-model="email"
         />
         <div class="fv-plugins-message-container">
           <div class="fv-help-block">
@@ -104,87 +83,14 @@
       </div>
       <!--end::Input group-->
 
-      <!--begin::Input group-->
-      <div class="mb-10 fv-row" data-kt-password-meter="true">
-        <!--begin::Wrapper-->
-        <div class="mb-1">
-          <!--begin::Label-->
-          <label class="form-label fw-bold text-dark fs-6"> Password </label>
-          <!--end::Label-->
-
-          <!--begin::Input wrapper-->
-          <div class="position-relative mb-3">
-            <Field
-              class="form-control form-control-lg form-control-solid"
-              type="password"
-              placeholder=""
-              name="password"
-              autocomplete="off"
-            />
-            <div class="fv-plugins-message-container">
-              <div class="fv-help-block">
-                <ErrorMessage name="password" />
-              </div>
-            </div>
-          </div>
-          <!--end::Input wrapper-->
-          <!--begin::Meter-->
-          <div
-            class="d-flex align-items-center mb-3"
-            data-kt-password-meter-control="highlight"
-          >
-            <div
-              class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"
-            ></div>
-            <div
-              class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"
-            ></div>
-            <div
-              class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"
-            ></div>
-            <div
-              class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"
-            ></div>
-          </div>
-          <!--end::Meter-->
-        </div>
-        <!--end::Wrapper-->
-        <!--begin::Hint-->
-        <div class="text-muted">
-          Use 8 or more characters with a mix of letters, numbers & symbols.
-        </div>
-        <!--end::Hint-->
-      </div>
-      <!--end::Input group--->
-
-      <!--begin::Input group-->
-      <div class="fv-row mb-5">
-        <label class="form-label fw-bold text-dark fs-6"
-          >Confirm Password</label
-        >
-        <Field
-          class="form-control form-control-lg form-control-solid"
-          type="password"
-          placeholder=""
-          name="password_confirmation"
-          autocomplete="off"
-        />
-        <div class="fv-plugins-message-container">
-          <div class="fv-help-block">
-            <ErrorMessage name="password_confirmation" />
-          </div>
-        </div>
-      </div>
-      <!--end::Input group-->
-
-      <!--begin::Input group-->
       <div class="fv-row mb-10">
         <label class="form-check form-check-custom form-check-solid">
           <Field
             class="form-check-input"
             type="checkbox"
-            name="toc"
-            value="1"
+            name="checkbox"
+            value="true"
+            v-model="agcheck"
           />
           <span class="form-check-label fw-semobold text-gray-700 fs-6">
             I Agree &
@@ -192,7 +98,6 @@
           </span>
         </label>
       </div>
-      <!--end::Input group-->
 
       <!--begin::Actions-->
       <div class="text-center">
@@ -227,6 +132,7 @@ import { useAuthStore, type User } from "@/stores/auth";
 import { useRouter } from "vue-router";
 import { PasswordMeterComponent } from "@/assets/ts/components";
 import Swal from "sweetalert2/dist/sweetalert2.js";
+import ApiService from "@/core/services/ApiService";
 
 export default defineComponent({
   name: "sign-up",
@@ -235,22 +141,34 @@ export default defineComponent({
     VForm,
     ErrorMessage,
   },
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      agcheck: "",
+
+    };
+  },
   setup() {
+    const username = ref('');
+    const email = ref('');
+    const password = ref('');
+    var agcheck = ref('');
+  
+  
+
     const store = useAuthStore();
     const router = useRouter();
 
     const submitButton = ref<HTMLButtonElement | null>(null);
 
     const registration = Yup.object().shape({
-      first_name: Yup.string().required().label("Name"),
-      last_name: Yup.string().required().label("Surname"),
+      username: Yup.string().min(4).max(20).required().label("Username"),
       email: Yup.string().min(4).required().email().label("Email"),
-      password: Yup.string().required().label("Password"),
-      password_confirmation: Yup.string()
-        .required()
-        .oneOf([Yup.ref("password"), null], "Passwords must match")
-        .label("Password Confirmation"),
     });
+
+    
 
     onMounted(() => {
       nextTick(() => {
@@ -259,28 +177,44 @@ export default defineComponent({
     });
 
     const onSubmitRegister = async (values: any) => {
-      values = values as User;
+      
+      let dataError;
+      let status = 200;
 
-      // Clear existing errors
-      store.logout();
+      const length = 10;
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let result = '';
 
-      // eslint-disable-next-line
-      submitButton.value!.disabled = true;
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex); 
+      }
 
-      // Activate indicator
-      submitButton.value?.setAttribute("data-kt-indicator", "on");
+      const data = await ApiService.post("http://202.129.16.94:82/api/register",
+      {
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        
+      }
+    ).catch((reason) => {
+      dataError = reason.response.data;
+      status = reason.response.status;
+      
+      });
+      console.log(typeof data === "undefined");
+      console.log("username.value =" + username.value)
+      console.log("email.value =" + email.value)
 
-      // Send login request
-      await store.register(values);
+      password.value = result
+      console.log("password.value =" + password.value);
 
-      const error = Object.values(store.errors);
-
-      if (!error) {
+      if (username !== null && email !== null && status == 400) {
         Swal.fire({
-          text: "You have successfully logged in!",
+          html: "<br> Create An Account Complete! </br>  Your Password is "+ " " + password.value,
           icon: "success",
           buttonsStyling: false,
-          confirmButtonText: "Ok, got it!",
+          confirmButtonText: "Next",
           heightAuto: false,
           customClass: {
             confirmButton: "btn fw-semobold btn-light-primary",
@@ -291,7 +225,7 @@ export default defineComponent({
         });
       } else {
         Swal.fire({
-          text: error[0] as string,
+          text: "Error!",
           icon: "error",
           buttonsStyling: false,
           confirmButtonText: "Try again!",
@@ -301,17 +235,64 @@ export default defineComponent({
           },
         });
       }
+      // values = values as User;
 
-      submitButton.value?.removeAttribute("data-kt-indicator");
-      // eslint-disable-next-line
-      submitButton.value!.disabled = false;
+      // // Clear existing errors
+      // store.logout();
+
+      // // eslint-disable-next-line
+      // submitButton.value!.disabled = true;
+
+      // // Activate indicator
+      // submitButton.value?.setAttribute("data-kt-indicator", "on");
+
+      // // Send login request
+      // await store.register(values);
+
+      // const error = Object.values(store.errors);
+
+      // if (!error) {
+      //   Swal.fire({
+      //     text: "You have successfully logged in!",
+      //     icon: "success",
+      //     buttonsStyling: false,
+      //     confirmButtonText: "Ok, got it!",
+      //     heightAuto: false,
+      //     customClass: {
+      //       confirmButton: "btn fw-semobold btn-light-primary",
+      //     },
+      //   }).then(function () {
+      //     // Go to page after successfully login
+      //     router.push({ name: "dashboard" });
+      //   });
+      // } else {
+      //   Swal.fire({
+      //     text: error[0] as string,
+      //     icon: "error",
+      //     buttonsStyling: false,
+      //     confirmButtonText: "Try again!",
+      //     heightAuto: false,
+      //     customClass: {
+      //       confirmButton: "btn fw-semobold btn-light-danger",
+      //     },
+      //   });
+      // }
+
+      // submitButton.value?.removeAttribute("data-kt-indicator");
+      // // eslint-disable-next-line
+      // submitButton.value!.disabled = false;
     };
 
     return {
+      username,
+      email,
       registration,
+      agcheck,
       onSubmitRegister,
       submitButton,
       getAssetPath,
+
+
     };
   },
 });
