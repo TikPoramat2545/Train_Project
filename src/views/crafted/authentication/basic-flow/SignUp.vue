73898ded07f93299b95bc9e284a6df4b59ee48a8
Poramat <porramut255780@gmail.com>
2023-05-30 10:15:43 +0700

    Train_Project

--- src/views/crafted/authentication/basic-flow/SignUp.vue
+++ src/views/crafted/authentication/basic-flow/SignUp.vue
@@ -184,7 +184,9 @@ export default defineComponent({
       const length = 10;
       const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       let result = '';
-      if (checked.value) {
+
+
+    if (checked.value) {
       for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         result += characters.charAt(randomIndex); 
